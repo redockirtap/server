bce81ac792f85460cd2ce826a9515e2a8e45b260
Partrickoder <redockirtap@gmail.com>
2022-11-22 16:20:50 +0200

    Add dotenv to store client secret and use axios instead of node-fetch.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express"); // framework for nodejs
 const cors = require("cors"); // to connect with front end that is in another location
+const axios = require("axios");
 const fetch = (...args) => import("node-fetch").then(({default: fetch}) => fetch(...args));
-
-
+require('dotenv').config();
+const PORT = 4000;
 const CLIENT_ID = "2e7940c77b22ef261b29";
-const CLIENT_SECRET = "efb0f6c5178cdae4336c78047ca8a2bf3f7e8ff6";
 
+console.log(process.env.CLIENT_SECRET)
 const app = express();
 app.use(cors());
 app.use(express.json()); // body-parser is deprecated; can use express.json()
@@ -13,17 +14,15 @@ app.use(express.json()); // body-parser is deprecated; can use express.json()
 // code from front end
 app.get("/getAccessToken", async function (req, res) {
 
-    const clientRequest = `?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`;
+    const clientRequest = `?client_id=${CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.query.code}`;
 
-    await fetch(`https://github.com/login/oauth/access_token${clientRequest}`, {
+    await axios.request(`https://github.com/login/oauth/access_token${clientRequest}`, {
         method: "POST",
         headers: {
             "Accept": "application/json",
         }
     }).then((response) => {
-        return response.json();
-    }).then((data) => {
-        return res.json(data); // response to front end
+        return res.json(response.data); // response to front end
     });
 
 });
@@ -31,27 +30,24 @@ app.get("/getAccessToken", async function (req, res) {
 app.get("/getUserProfile", async function (req, res) {
     req.get("Authorization");
     
-    await fetch("https://api.github.com/user", {
+    await axios.request("https://api.github.com/user", {
         method: "GET",
         headers: {
             "Authorization": req.get("Authorization"),
         }
-    }).then(response => response.json()).then(data => {
-        console.log(data);
-        return res.json(data);
-    });
+    }).then(response => res.json(response.data))
 })
 
 app.get("/getUserRepos", async function (req, res) {
     req.get("Authorization");
-    await fetch("https://api.github.com/user/repos", {
+    await axios.request("https://api.github.com/user/repos", {
         method: "GET",
         headers: {
             "Authorization": req.get("Authorization"),
         }
-    }).then(response => response.json()).then(data => {res.set("Cache-Control", "public"); res.json(data)});
+    }).then(response => res.json(response.data));
 })
 
-app.listen(4000, function () {
+app.listen(PORT, function () {
     console.log('it works!')
 })
